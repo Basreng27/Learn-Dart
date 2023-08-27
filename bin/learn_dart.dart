7b067ebbb731c306aa594b2eec6d4b5d8cd796d1
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-08-27 12:06:10 +0700

    LEARN : Input In Dart

--- bin/learn_dart.dart
+++ bin/learn_dart.dart
@@ -1,3 +1,16 @@
+import 'dart:io'; // Untuk Membuat input Output
+
+// Main dari dart, Seperti Index
 void main(List<String> arguments) {
-  print('First Dart');
+  // untuk Inputnya
+  // var = untuk pembuatan variablenya
+  // stdin = Standar Input. yang mana "readLineSync()" ini akan membaca 1 line inputan yang dikirimkan berupa string
+  var input = stdin.readLineSync();
+  // deklarasi number dengan string 10
+  var number = "10";
+  // lalu di konversikan sehingga hasilnya integer
+  var conversion = int.tryParse(number);
+
+  print(input);
+  print(conversion);
 }
